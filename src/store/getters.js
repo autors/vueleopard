8c463bb40ae03482a4a2e4d2cded3dcf3026ff1f
niyuelin1990 <niyuelin1990@163.com>
2017-11-10 09:37:45 +0800

    git page

--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  userInfo: state => state.user.userInfo,
-  permission_routers: state =>state.app.routers,
-}
-export default getters
--- src/views/layout/Bottom.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="bottom-pages">
-    2011-2016 &copy; WANGTING
-  </div>
-</template>
-
-<script>
-</script>
